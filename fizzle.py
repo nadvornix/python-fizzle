d0b2b60290be7892cc71d8b319ac3c35b634e38c
Jiri Nadvornik <nadvornik.jiri@gmail.com>
2012-12-16 16:49:40 +0100

    pick_one, pick_N

--- fizzle.py
+++ fizzle.py
@@ -5,9 +5,9 @@
 
 '''
 
-def DL_distance(s1,s2,substitutions=[],symetric=True):
+def dl_distance(s1,s2,substitutions=[],symetric=True):
 	"""
-	Return DL distance between s1 and s2.
+	Return DL distance between s1 and s2. Default cost of substitution, insertion, deletion and transposition is 1
 	substitutions is list of tuples of characters (what, substituted by what, cost), 
 		maximal value of substitution is 2 (ie: cost deletion & insertion that would be otherwise used)
 		eg: substitutions=[('a','e',0.4),('i','y',0.3)]
@@ -21,11 +21,8 @@ def DL_distance(s1,s2,substitutions=[],symetric=True):
 		if symetric:
 			subs[(b,a)]=v
 	
-	#import array
 	row=(1+len(s2))*[0.]
 	matrix=[[i+j for j in range(len(s2)+1)] for i in range(len(s1)+1)]	#matrix |s1|+1 x |s2|+1
-	# print matrix
-	# print len(matrix),len(row)
 
 	for i in range(len(s1)):
 		for j in range(len(s2)):
@@ -34,10 +31,7 @@ def DL_distance(s1,s2,substitutions=[],symetric=True):
 				cost = 0
 			else:
 				cost = subs[(ch1,ch2)]
-	# 		print s1[i],s2[j],[matrix[i][j+1]+1, 	#deletion
-	# 						  matrix[i+1][j]+1,	#insertion
-	# 						  matrix[i][j]+cost	#substitution or no change
-	# 						]
+
 			matrix[i+1][j+1]=min([matrix[i][j+1]+1, 	#deletion
 							  matrix[i+1][j]+1,	#insertion
 							  matrix[i][j]+cost	#substitution or no change
@@ -47,31 +41,40 @@ def DL_distance(s1,s2,substitutions=[],symetric=True):
 				matrix[i+1][j+1] = min([matrix[i+1][j+1],
 									matrix[i-1][j-1]+cost])
 
-	print "     ",
-	for i in s2:
-		print i,"",
-	print 
-	for i,r in enumerate(matrix):
-		if i==0:
-			print " ",r
-		else:
-			print s1[i-1],r
+	# print "     ",
+	# for i in s2:
+	# 	print i,"",
+	# print 
+	# for i,r in enumerate(matrix):
+	# 	if i==0:
+	# 		print " ",r
+	# 	else:
+	# 		print s1[i-1],r
 	return matrix[-1][-1]
 
-def match_list(s, l):
+def match_list(s, l, **kw):
 	'''
 	returns list of elements of l with each element having assigned distance from s
 	'''
-	return map( lambda x:(DL_distance(s,x),x), l)
+	return map( lambda x:(dl_distance(s,x,**kw),x), l)
 
-def pick_one(s, options, num=1):
-	''' picks one string from options best matching with s 
-		- if num is set then returns top num results instead of one
+def pick_N(s, l, num=3,**kw):
+	''' picks top N string from options best matching with s 
+		- if num is set then returns top num results instead of default three
 	'''
-	pass #TODO
+	return sorted(match_list(s,l, **kw))[:num]
 
+def pick_one(s,l,**kw):
+	try:
+		return pick_N(s,l,1,**kw)[0]
+	except IndexError:
+		return None
 
 if __name__=="__main__":
-	print DL_distance('dayme', 'dayne', substitutions=[('a','e',0.4),('i','y',0.3),('m','n',0.5)])
+	commonErrors=[('a','e',0.4),('i','y',0.3),('m','n',0.5)]
+	misspellings = ["Levenshtain","Levenstein","Levinstein","Levistein","Levemshtein"]
 
-	# print match_list("Levenshtein", ["Levenshtain","Levenstein","Levinstein","Levistein"])
+	print dl_distance('dayme', 'dayne', substitutions=commonErrors)
+	print match_list("Levenshtein", misspellings, substitutions=commonErrors)
+	print pick_N("Levenshtein", misspellings, 2, substitutions=commonErrors)
+	print pick_one("Levenshtein", misspellings, substitutions=commonErrors)
